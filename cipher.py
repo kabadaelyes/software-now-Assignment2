1a98d7bc1af2a3320a06539f959e791945f628d5
kabadaelyes <kabadaelyes@gmail.com>
2026-08-23 19:40:23 +0930

    Set up encryption file processing

---
+++ cipher.py
@@ -0,0 +1,11 @@
+def encrypt_file(shift1, shift2, input_path, output_path):
+    with open(input_path, "r") as file:
+      content = file.read()
+    for char in content :
+       if char.islower() :
+          if "a" <= char <= "n" :
+             
+             
+       
+
+    
