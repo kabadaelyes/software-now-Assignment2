043b33207a09f528ea5b005c91fc4de58a0f3e54
kabadaelyes <kabadaelyes@gmail.com>
2026-08-27 14:29:47 +0930

    Implement encryption and decryption

--- cipher.py
+++ cipher.py
@@ -6,15 +6,15 @@ def encrypt_file(shift1, shift2, input_path, output_path):
     for char in content :
        if char.islower():
           if "a" <= char <= "n" :
-             encrypted_content += chr((((ord(char)-97) + (shift1 * shift2) )% 26)+ 97) 
+             encrypted_content += chr((((ord(char)-97) + (shift1 * shift2) )% 14)+ 97) 
           else:
-             encrypted_content += chr((((ord(char)-97) - (shift1 + shift2) )% 26)+ 97) 
+             encrypted_content += chr((((ord(char)-111) - (shift1 + shift2) )% 12)+ 111) 
 
        elif char.isupper():
           if "A" <= char <= "M" :
-             encrypted_content += chr((((ord(char)-65) - shift1 )% 26)+ 65)
+             encrypted_content += chr((((ord(char)-65) - shift1 )% 13)+ 65)
           else:
-             encrypted_content += chr((((ord(char)-65) + (shift2 ** 2 ))% 26)+ 65) 
+             encrypted_content += chr((((ord(char)-78) + (shift2 ** 2 ))% 13)+ 78) 
 
        elif char.isdigit():
            encrypted_content += chr((((ord(char)-48) + (shift1 - shift2 ))% 10)+ 48) 
@@ -30,4 +30,25 @@ def decrypt_file(shift1, shift2, input_path, output_path):
     decrypted_content = ""
     with open(input_path, "r") as file:
       content = file.read()
+    for char in content :
+           if char.islower():
+              if "a" <= char <= "n" :
+                 decrypted_content += chr((((ord(char)-97) - (shift1 * shift2) )% 14)+ 97) 
+              else:
+                 decrypted_content += chr((((ord(char)-111) + (shift1 + shift2) )% 12)+ 111) 
     
+           elif char.isupper():
+              if "A" <= char <= "M" :
+                 decrypted_content += chr((((ord(char)-65) + shift1 )% 13)+ 65)
+              else:
+                 decrypted_content += chr((((ord(char)-78) - (shift2 ** 2 ))% 13)+ 78) 
+    
+           elif char.isdigit():
+               decrypted_content += chr((((ord(char)-48) - (shift1 - shift2 ))% 10)+ 48) 
+           else: 
+              decrypted_content += char
+    with open(output_path, "w") as file:
+     file.write(decrypted_content)
+
+
+
